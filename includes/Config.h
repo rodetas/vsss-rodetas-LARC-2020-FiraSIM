32930b69ab4b8eac6e628440d791295faecda194
rleonardorezende@gmail.com <rleonardorezende@gmail.com>
2018-05-09 17:49:36 -0300

    CamelCase

---
+++ includes/Config.h
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the VSS-SampleStrategy project.
+ *
+ * This Source Code Form is subject to the terms of the GNU GENERAL PUBLIC LICENSE,
+ * v. 3.0. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
+ */
+
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#include <Boost.h>
+#include <sstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Config{
+
+public:
+    static bool debug;
+    static bool realEnvironment;
+    static bool changeSide;
+    static string teamColor;
+    static bool argumentParse(int ,char**);
+
+   
+};
+#endif // _CONFIG_H_
---
+++ src/Movimentation.cpp
@@ -0,0 +1,75 @@
+#include "Movimentation.h"
+
+
+/*
+ * calculates the basic movimentation to goal to target
+ */
+Command Movimentation::movePlayers(RobotState robot, btVector3 target){
+
+	Command command;
+
+	// movement along the field
+	if (robot.cosFrom(target) < -0.4) {
+		command = definePwm(robot, target, 'F');
+
+	} else if (robot.cosFrom(target) > 0.4){
+		command = definePwm(robot, target, 'B');
+
+	} else {
+		if (robot.sinFrom(target) > 0) {
+			command = turnRight(20, 20);
+		} else {
+			command = turnLeft(20, 20);
+		}
+	}
+
+    return command;
+}
+
+/*
+ * Correct robot pwm to follow the destination
+ */
+Command Movimentation::definePwm(RobotState robot, btVector3 target, char direction){
+
+	int standardPower = 50;
+	int basePower = standardPower * 1;//robot.get_potency_factor();
+	int correctionPower = standardPower * robot.sinFrom(target) * 0.7;//robot.get_curve_factor();
+	int pwmMotor1 = (basePower - correctionPower);
+	int pwmMotor2 = (basePower + correctionPower);
+
+	if(direction == 'B'){
+		pwmMotor1 = pwmMotor1 * (-1);
+		pwmMotor2 = pwmMotor2 * (-1);
+	}
+
+	Command verifiedPwm = checkPwm(Command(pwmMotor1, pwmMotor2));
+
+	return verifiedPwm;
+}
+
+Command Movimentation::checkPwm(const Command& pwm){
+    Command command(pwm);
+
+    if(pwm.left  > 255) command.left  = 255;
+    if(pwm.right > 255) command.right = 255;
+
+    if(pwm.left  < -255) command.left  = -255;
+    if(pwm.right < -255) command.right = -255;
+
+    return command;
+}
+
+Command Movimentation::turnLeft(int pwm1, int pwm2){
+	Command command(-pwm1, pwm2);
+	return command;
+}
+
+Command Movimentation::turnRight(int pwm1, int pwm2){
+	Command command(pwm1, -pwm2);
+	return command;
+}
+
+Command Movimentation::stop(){
+	Command command(0, 0);
+	return command;
+}
