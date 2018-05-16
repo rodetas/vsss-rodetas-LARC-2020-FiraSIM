d5c5b00d581cbb42970dccb9734cc295e69b5265
samuelmp34 <samuel.smp34@live.com>
2018-05-16 12:10:17 -0300

    tag de troca de jogadores

--- src/Config.cpp
+++ src/Config.cpp
@@ -20,7 +20,7 @@ bool Config::argumentParse(int argc, char** argv) {
         ("debug,d", "(Optional) enables the debug rotine")
         ("environment,e", "(Optional) set real environment")
         ("rotate,r", "(Optional) rotate robots positions")
-	("swap,s", bpo::value<std::string>()->default_value("on"), "(Optional) Turn On/off the player's swap.") 
+	("swap,s", "(Optional) Turn On/off the player's swap.") 
         ("color,c", bpo::value<std::string>()->default_value("blue"), "(Optional) Specify the main color of your team, may be yellow or blue.");
     bpo::variables_map vm;
     bpo::store(bpo::parse_command_line(argc, argv, desc), vm);
@@ -31,9 +31,9 @@ bool Config::argumentParse(int argc, char** argv) {
         return false;
     }
     
-    if (vm["swap"].as<string>() == "off"){
-		playersSwap= false;
-	}
+    if (vm.count("swap")){
+	playersSwap= false;
+    }
 		
 
     changeSide = (bool) vm.count("rotate");
