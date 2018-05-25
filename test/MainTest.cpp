3a6304a76c0992d7248854dcd46a637d1a52bffe
samuelmp34 <38567098+samuelmp34@users.noreply.github.com>
2018-05-25 01:14:37 -0300

    Update MainTest.cpp

--- test/MainTest.cpp
+++ test/MainTest.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 using namespace common;
 int main(int argc, char** argv){
-		transmissionTests transmissionTests;
+		TransmissionTests transmissionTests;
 			
 		transmissionTests.testGenerateMessage();
 		transmissionTests.testCheckSum();
