69d54d3478e376f8b710779b5d3e65b3c79fd317
Manoel Stilpen <mstilpen15@gmail.com>
2018-05-25 01:30:23 -0300

    refact - pequenas melhorias

--- test/MainTest.cpp
+++ test/MainTest.cpp
@@ -1,17 +1,14 @@
 //
 // Created by manoel on 07/05/18.
 //
-#include "TransmissionTests.h"
-#include <iostream>
-#include <Config.h>
-#include <Common.h>
 
-using namespace std;
-using namespace common;
+#include "TransmissionTests.h"
+
 int main(int argc, char** argv){
-		TransmissionTests transmissionTests;
-			
-		transmissionTests.testGenerateMessage();
-		transmissionTests.testCheckSum();
+    TransmissionTests transmissionTests;
+
+    transmissionTests.testGenerateMessage();
+    transmissionTests.testCheckSum();
+    
 	return 0;
 }
