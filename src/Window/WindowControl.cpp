0a3d6a7ee32547edf19c33f46de080dd091e29a5
Manoel Stilpen <mstilpen15@gmail.com>
2018-06-20 20:18:59 -0300

    window improvements

--- src/Window/WindowControl.cpp
+++ src/Window/WindowControl.cpp
@@ -5,8 +5,6 @@
 #include "Window/WindowControl.h"
 
 void WindowControl::start() {
-
-
     initializeWidgets();
 	setSignals();
 
@@ -51,17 +49,15 @@ void WindowControl::setSignals(){
 }
 
 void WindowControl::onPressButtonPlaying(Gtk::ToggleButton * buttonPlay){
-    // invocar o signal para atualizar o kernel que houve uma variavel
-    //signalUpdatePlaying.emit( valor )
-	//bool state;
+    if(buttonPlay->get_active()) buttonPlay->set_label("Sending");
+    else buttonPlay->set_label("Paused");
 
-	//state=Gtk::ToggleButton::get_active();
 	signalUpdatePlaying.emit(buttonPlay->get_active());
-
 }
 
 void WindowControl::onPressButtonTesting(Gtk::ToggleButton * buttonTests){
-    // invocar o signal para atualizar o kernel que houve uma na variavel
-    //signalUpdateTesting.emit ...
+    if(buttonTests->get_active()) buttonTests->set_label("Testing");
+    else buttonTests->set_label("Stopped");
+
 	signalUpdateTesting.emit(buttonTests->get_active());
 }
