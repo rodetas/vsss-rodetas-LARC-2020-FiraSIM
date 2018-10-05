21b72ab1a3d1689d45f789618967020917d25d34
samuelmp34 <samuel.smp34@live.com>
2018-10-05 15:27:52 -0300

    refact

---
+++ includes/Strategies/RobotStrategyCenterAttack.h
@@ -0,0 +1,20 @@
+//
+//created by Samuel on 04/10/18
+//
+
+#ifndef SDK_RODETAS_ROBOTSTRATEGYCENTERATTACK_H
+#define SDK_RODETAS_ROBOTSTRATEGYCENTERATTACK_H
+
+#include <Strategies/RobotStrategy.h>
+#include <iostream>
+
+class RobotStrategyCenterAttack : public RobotStrategy {
+
+public:
+     RobotStrategyCenterAttack();
+    vss::WheelsCommand specificStrategy(vss::WheelsCommand) override;
+    vss::Pose defineTargetAndArrivalOrientation() override;
+    float applyUnivectorField(vss::Pose) override;
+};
+
+#endif //SDK_RODETAS_ROBOTSTRATEGYCENTERATTACK_H
---
+++ src/Strategies/RobotStrategyForwardAttack.cpp
@@ -0,0 +1,37 @@
+//
+//Created by Samuel on 28/09/18
+//
+#include <Strategies/RobotStrategyForwardAttack.h>
+#include <iostream>
+RobotStrategyForwardAttack::RobotStrategyForwardAttack() = default;
+
+vss::WheelsCommand RobotStrategyForwardAttack::specificStrategy(vss::WheelsCommand c) {
+    c = cornerStrategy(c);
+    c = stopStrategy(c);
+    return c;
+}
+
+vss::Pose RobotStrategyForwardAttack::defineTargetAndArrivalOrientation(){
+    vss::Pose target;
+    vss::Point centerGoal = vss::Point(0, vss::MAX_COORDINATE_Y/2);
+    target.x = centerGoal.x;
+    target.y = centerGoal.y;
+    
+
+
+    return target;
+  
+}
+
+
+float RobotStrategyForwardAttack::applyUnivectorField(vss::Pose target){
+    std::vector<std::pair<vss::Point, vss::Point>> obstacles;
+    for (auto &r: state.robots) {
+        if ((r.position.x != robot.position.x) && (r.position.y != robot.position.y)){
+            obstacles.push_back(std::make_pair(r.position, r.vectorSpeed));
+        }
+    }
+    UnivectorField univectorField;
+    path = univectorField.drawPath(robot, target, obstacles);
+    return univectorField.defineFi(robot, target, obstacles);
+}
