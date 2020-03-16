2f4a88ebf44d329ee0205b7ec71b6ae6500ab1a6
Mateus Jorge <52360561+mavismg@users.noreply.github.com>
2020-03-16 14:02:27 -0300

    Last commit

--- src/Strategies/RobotStrategy.cpp
+++ src/Strategies/RobotStrategy.cpp
@@ -25,43 +25,25 @@ vss::WheelsCommand RobotStrategy::applyStrategy(RobotState r, RodetasState s, Ro
     return command;
 }
 
-//
 vss::WheelsCommand RobotStrategy::cornerStrategy(vss::WheelsCommand c) {
 
     if (strategyBase.isBoard() && strategyBase.isStopped()) {
 
         // girar caso robo esteja preso de frente pra parede
         if (robot.cosFrom(state.ball.position) > -0.9 && robot.cosFrom(state.ball.position) < 0.9) {
-
-            //robot.setAngle(75);
-
             if (robot.sinFrom(state.ball.position) > 0) {
-                // c = movimentation.turnRight(50, 50);
-                c = movimentation.stop();
-                strategyBase.isBlocked(); // Giradinha para ganhar espaço e armar a jogada
-                // Entao ...
-                c = movimentation.turnRight(75, 35);
+                c = movimentation.turnRight(50, 30);
             } else {
-                // c = movimentation.turnLeft(50, 50);
-                c = movimentation.stop();
-                strategyBase.isBlocked();
-                // Entao ...
-                c = movimentation.turnLeft(75, 35);
+                c = movimentation.turnLeft(50, 50);
             }
 //            cout << "preso pra parede" << endl;
         }
 
         // girar caso robo prenda a bola na parede - 8 cm
-        if (robot.distanceFrom(state.ball.position) < (15)) {
+        if (robot.distanceFrom(state.ball.position) < (8)) {
             if (robot.position.y < (vss::MAX_COORDINATE_Y / 2)) {
-                c = movimentation.stop();
-                strategyBase.isBlocked(); // Giradinha para ganhar espaço e armar a jogada
-                // Entao ...
                 c = movimentation.turnLeft(60, 60);
             } else {
-                c = movimentation.stop();
-                strategyBase.isBlocked();
-                // Entao ...
                 c = movimentation.turnRight(60, 60);
             }
 //            cout << "preso com bola" << endl;
@@ -71,18 +53,17 @@ vss::WheelsCommand RobotStrategy::cornerStrategy(vss::WheelsCommand c) {
     return c;
 }
 
-//
 vss::WheelsCommand RobotStrategy::stopStrategy(vss::WheelsCommand c) {
     // Para o robo quando atinge o target, alem disso, rotaciona de forma que esteja sempre virado para a bola
 
     float maxDistance = 12; // 12 cm
     float distanceTarget = robot.distanceFrom(target);
 
-    /*REVER VELOCIDADE
+/* 	REVER VELOCIDADE
 	if(robot.getVelocity() > image_size.x * (0.05)){
 		maxDistance = 24; // 24 cm
-	}*/
-
+	}
+*/
 
     if (distanceTarget < maxDistance) {
         c.leftVel = c.leftVel * (distanceTarget / maxDistance);
@@ -97,10 +78,8 @@ vss::WheelsCommand RobotStrategy::stopStrategy(vss::WheelsCommand c) {
         } else {
 
             if (robot.sinFrom(state.ball.position) > 0) {
-                // c = movimentation.turnRight(10, 10); <- Anterior
                 c = movimentation.turnRight(10, 10);
             } else {
-                // c = movimentation.turnLeft(10, 10); <- Anterior
                 c = movimentation.turnLeft(10, 10);
             }
         }
@@ -109,21 +88,11 @@ vss::WheelsCommand RobotStrategy::stopStrategy(vss::WheelsCommand c) {
     return c;
 }
 
-/* Descobrir oque esse kickStrategy esta fazendo! */
-
 vss::WheelsCommand RobotStrategy::kickStrategy(vss::WheelsCommand c) {
 
-    /* Essa parte eu fiz apenas para ver como os robos iriam reagir*/
-
-    float distanceTarget = robot.distanceFrom(target);
-
-    if (distanceTarget < 4) {
-        
-        if (robot.sinFrom(state.ball.position) > 0) {
-            c = movimentation.turnLeft(60, 60);
-        } else {
-            c = movimentation.turnRight(60, 60);
-        }
+    //SE O ROBO ESTIVER PERTO DO GOL, VAI RAPIDO EM DIREÇAO A TAL
+    if (strategyBase.isParallelToGoal()) {
+        c = movimentation.movePlayers(this->robot, 0.8, RobotSpeed::FAST);
     }
 
     return c;
@@ -131,18 +100,22 @@ vss::WheelsCommand RobotStrategy::kickStrategy(vss::WheelsCommand c) {
 
 vss::WheelsCommand RobotStrategy::blockedStrategy(vss::WheelsCommand c) {
 
-    /* A ideia aqui era fazer os robos sairem mais rapido de uma marcacao */
-
+    float minDistance = 1; //1 cm
+    float distanceTarget = robot.distanceFrom(target);
+    //???
+    if (distanceTarget < minDistance) {
+        c.leftVel = c.leftVel * (distanceTarget / minDistance);
+        c.rightVel = c.rightVel * (distanceTarget / minDistance);
+    }
+    //GIRAR ROBO
     if (strategyBase.isBlocked() && strategyBase.isStopped()) {
-
-        c = movimentation.turnRight(180, 180);
-
-        if (strategyBase.isBlocked() && strategyBase.isStopped()) {
-
-            c = movimentation.turnLeft(270, 270);
-
-        }
-
+        if (distanceTarget < 0.5) {
+            c = movimentation.turnLeft(50, 50);
+        } else {
+            c = movimentation.turnRight(50, 50);
+        }     
+    } else {
+        c = movimentation.stop();
     }
 
     return c;
