271a5b355c9116eac1c4d63fd28910a45703e90e
samuelmpedroza <samuelmpedroza@gmail.com>
2020-11-11 11:27:35 -0300

    Dia 1 Oficial

--- runRodetas.sh
+++ runRodetas.sh
@@ -1,15 +1,2 @@
-team = ""
-    while getopts "h?tc:" opt; do
-    case "$opt" in
-    h|\?)
-        show_help
-        exit 0
-        ;;
-    t)  verbose=1
-        ;;
-    c)  team=$OPTARG
-        ;;
-    esac
-done
     cd build
-    sudo ./SDK-Rodetas -c team
+    sudo ./SDK-Rodetas -c $1
